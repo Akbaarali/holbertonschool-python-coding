0bdfd941102f622031150621bbd77beacf768f9a
Akbaarali <12716@holbertonstudents.com>
2025-12-14 09:11:20 +0000

    Add python-classes project with Square task 0

---
+++ python-classes/0-square.py
@@ -0,0 +1,28 @@
+  GNU nano 6.2                                                                                        0-square.py                                                                                                 
+#!/usr/bin/python3
+"""Defines a Square class."""
+
+
+class Square:
+    """Represents a square."""
+
+    def __init__(self, size):
+        self.__size = size
+
+
+
+
+
+
+
+
+
+#!/usr/bin/python3
+"""Defines a Square class."""
+
+
+class Square:
+    """Represents a square."""
+
+    def __init__(self, size):
+        self.__size = size
